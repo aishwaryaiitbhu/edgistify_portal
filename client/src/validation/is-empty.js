cca96e09059363fc2c829aa0351f4f6648e80571
aishwaryaiitbhu <30286085+aishwaryaiitbhu@users.noreply.github.com>
2020-02-15 09:57:13 +0530

    Add files via upload

---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,7 @@
+//custom method to check an empty string(to check whether there are any error messages accumulated)
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+export default isEmpty;
