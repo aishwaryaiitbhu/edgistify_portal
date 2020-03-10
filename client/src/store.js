5771328058b4768d6aa26ef4d516120d0d4291df
aishwaryaiitbhu <30286085+aishwaryaiitbhu@users.noreply.github.com>
2020-03-10 19:17:40 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,15 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-const initialState = {};
-const middleware = [thunk];
+import { createStore, applyMiddleware, compose } from "redux";//some libraries which help in creating storage for states of the application
+import thunk from "redux-thunk";//middleware for redux, allows our api requests to pass through the reducers
+import rootReducer from "./reducers"; //check the file for details
+const initialState = {};//initial state of the application
+const middleware = [thunk];//
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()//this line enables the browser to use the redux extension tools when the application is running
   )
-);
+);//creating the store for the application, which stores all the states
 
-export default store;
+export default store;// exporting the store which is created
