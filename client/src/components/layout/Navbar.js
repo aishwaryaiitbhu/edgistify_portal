5cd1db1966ca71f8bddcfc2b78da486ec12ba39f
aishwaryaiitbhu <30286085+aishwaryaiitbhu@users.noreply.github.com>
2020-03-11 11:40:22 +0530

    Update Navbar.js

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions";
-import { clearCurrentProfile } from "../../actions/profileActions";
+//This is the Navrbar component, it will have states so we will use class based component
+import React, { Component } from "react"; //all components created have to be inherited from Component class
+import { Link } from "react-router-dom";//Link to other components
+import PropTypes from "prop-types";//Defining attributes(props), already discussed the difference between props and states and why we need props
+import { connect } from "react-redux";// connect our component to the store which we have created
+import { logoutUser } from "../../actions/authActions";//an action which we need in this Navbar and we have to dispatch it
+import { clearCurrentProfile } from "../../actions/profileActions";//also an action which we need after logging out the current user
 
 class Navbar extends Component {
   onLogoutClick(e) {
@@ -83,14 +84,17 @@ class Navbar extends Component {
     );
   }
 }
+//defining props for Navbar component
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
+//mapping state to props for the same reason as out store is huge, clearly defining which states can be resued and we map that to props
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
 export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(
   Navbar
-);
+); //exporting the component created, it will be very beneficial if u see the syntax of 'connect' function, it takes two parameters,first the mapping of state to props, and second the actions which needs to be dispatched
+//An action means an event associated with a component, like what happends if we click the logout button(some components do have actions associated to them and some dont have, so the second parameter in ''connect for those components will be set to empty or null)
