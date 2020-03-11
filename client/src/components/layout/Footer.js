5e68b32cc03cce38c7d9d213f009c89427f51d96
aishwaryaiitbhu <30286085+aishwaryaiitbhu@users.noreply.github.com>
2020-03-11 09:24:22 +0530

    Update Footer.js

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+// a function based component because footer component has no states, so no need to use a class based component here
+//create a normal HTML to be rendered inside a functinal based component and export it
 export default function Footer() {
   return (
     <footer className='bg-dark text-white mt-5 p-4 text-center'>
